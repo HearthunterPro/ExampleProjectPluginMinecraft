d2c4c2776652371a5fbdcd0f91cf1ddf284b980f
CushyPro <homepo18@hotmail.com>
2021-03-03 21:05:25 +0700

    V 1.0.1

--- src/main/java/mc/CushyPro/PluginName/ConfigType.java
+++ src/main/java/mc/CushyPro/PluginName/ConfigType.java
@@ -6,83 +6,86 @@ import org.bukkit.Location;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
 
+@SuppressWarnings("ALL")
 public enum ConfigType {
 
-	A("Hello");
+    A("Test-Config", "hello world");
 
-	public static YamlConfiguration cfg;
+    public static YamlConfiguration cfg;
 
-	public static void LoadConfig(YamlConfiguration yaml) {
-		cfg = yaml;
-		for (ConfigType type : values()) {
-			if (!cfg.isSet(type.toString())) {
-				cfg.set(type.toString(), type.obj);
-			}
-		}
-	}
+    public static void LoadConfig(YamlConfiguration yaml) {
+        cfg = yaml;
+        for (ConfigType type : values()) {
+            if (!cfg.isSet(type.toString())) {
+                cfg.set(type.toString(), type.obj);
+            }
+        }
+    }
 
-	private Object obj;
+    private Object obj;
+    private String prefix;
 
-	ConfigType(Object obj) {
-		this.obj = obj;
-	}
+    ConfigType(String prefix, Object obj) {
+        this.prefix = prefix;
+        this.obj = obj;
+    }
 
-	@Override
-	public String toString() {
-		return super.toString().replace("_", ".");
-	}
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 
-	public Object get() {
-		if (cfg.isSet(toString())) {
-			return cfg.get(toString());
-		}
-		return obj;
-	}
+    public Object get() {
+        if (cfg.isSet(prefix)) {
+            return cfg.get(prefix);
+        }
+        return obj;
+    }
 
-	public String getString() {
-		return (String) get();
-	}
+    public String getString() {
+        return get().toString();
+    }
 
-	public int getInt() {
-		return (int) get();
-	}
+    public int getInt() {
+        return (Integer) get();
+    }
 
-	public double getDouble() {
-		return (double) get();
-	}
+    public double getDouble() {
+        return (Double) get();
+    }
 
-	public long getLong() {
-		return (long) get();
-	}
+    public long getLong() {
+        return (Long) get();
+    }
 
-	public float getFloat() {
-		return (float) get();
-	}
+    public float getFloat() {
+        return (Float) get();
+    }
 
-	@SuppressWarnings("unchecked")
-	public List<String> getStringList() {
-		return (List<String>) get();
-	}
+    @SuppressWarnings("unchecked")
+    public List<String> getStringList() {
+        return (List<String>) get();
+    }
 
-	public List<?> getList() {
-		return (List<?>) get();
-	}
+    public List<?> getList() {
+        return (List<?>) get();
+    }
 
-	public ItemStack getItemStack() {
-		return (ItemStack) get();
-	}
+    public ItemStack getItemStack() {
+        return (ItemStack) get();
+    }
 
-	public Location getLocation() {
-		return (Location) get();
-	}
+    public Location getLocation() {
+        return (Location) get();
+    }
 
-	@SuppressWarnings("unchecked")
-	public <T> T get(T def) {
-		return (get() != null) ? (T) get() : def;
-	}
-	
-	public boolean getBoolean() {
-		return (boolean) get();
-	}
+    @SuppressWarnings("unchecked")
+    public <T> T get(T def) {
+        return (get() != null) ? (T) get() : def;
+    }
+
+    public boolean getBoolean() {
+        return (Boolean) get();
+    }
 
 }
--- src/main/java/mc/CushyPro/PluginName/MessageType.java
+++ src/main/java/mc/CushyPro/PluginName/MessageType.java
@@ -5,57 +5,59 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 public enum MessageType {
 
-	YOUNOTHAVEPERMISSION("&aYou not have Permission!"),
+    YOUNOTHAVEPERMISSION("You-not-have-premission", "&aYou not have Permission!"),
+    ;
 
-	;
+    public static YamlConfiguration cfg;
 
-	public static YamlConfiguration cfg;
+    public static void LoadConfig(YamlConfiguration yaml) {
+        cfg = yaml;
+        for (MessageType type : values()) {
+            if (!cfg.isSet("MessageType." + type.getID())) {
+                cfg.set("MessageType." + type.getID(), type.msg);
+            }
+        }
+    }
 
-	public static void LoadConfig(YamlConfiguration yaml) {
-		cfg = yaml;
-		for (MessageType type : values()) {
-			if (!cfg.isSet("MessageType." + type.getID())) {
-				cfg.set("MessageType." + type.getID(), type.msg);
-			}
-		}
-	}
+    String msg;
+    String prefix;
 
-	private String msg;
+    MessageType(String prefix, String string) {
+        this.prefix = prefix;
+        msg = string;
+    }
 
-	MessageType(String string) {
-		msg = string;
-	}
+    public String getID() {
+        return prefix;
+    }
 
-	public String getID() {
-		return super.toString();
-	}
+    @Override
+    public String toString() {
+        return getColor(getMessage());
+    }
 
-	@Override
-	public String toString() {
-		return getColor(getMessage());
-	}
-	
-	String getMessage() {
-		if (cfg.isSet("MessageType." + getID())) {
-			return cfg.getString("MessageType." + getID());
-		}
-		return msg;
-	}
+    String getMessage() {
+        if (cfg.isSet(getID())) {
+            return cfg.getString(getID());
+        }
+        return msg;
+    }
 
-	public String toString(String... replace) {
-		String mxs = getMessage();
-		if (replace.length % 2 == 0) {
-			for (int x = 0; x < replace.length / 2; x++) {
-				String key = replace[x * 2];
-				String value = replace[(x * 2) + 1];
-				mxs = mxs.replace(key, value);
-			}
-		}
-		return getColor(mxs);
-	}
+    @SuppressWarnings("ALL")
+    public String toString(String... replace) {
+        String mxs = getMessage();
+        if (replace.length % 2 == 0) {
+            for (int x = 0; x < replace.length / 2; x++) {
+                String key = replace[x * 2];
+                String value = replace[(x * 2) + 1];
+                mxs = mxs.replace(key, value);
+            }
+        }
+        return getColor(mxs);
+    }
 
-	public String getColor(String msg) {
-		return ChatColor.translateAlternateColorCodes('&', msg);
-	}
+    public String getColor(String msg) {
+        return ChatColor.translateAlternateColorCodes('&', msg);
+    }
 
 }
